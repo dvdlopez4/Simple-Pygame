a482402ed975f51dea9ec9f63340f881b72d2933
David Lopez <dvdlopez4@gmail.com>
2018-05-23 16:29:52 -0700

    Extract out AI component

---
+++ src/DumbBot.py
@@ -0,0 +1,29 @@
+
+import pygame
+import time
+from math import *
+from physics import *
+from graphics import *
+from entity import *
+
+
+
+class DumbBot(object):
+    def __init__(self, world):
+        self.direction = 1
+        self.players = world.players
+        self.state = 0
+
+    def update(self, Entity):
+
+        Entity.velocity[0] = 100 * self.direction
+        if Entity.isOnGround:
+            Entity.velocity[1] = -250
+            self.direction = 1
+            if Entity.rect.x > self.players[0].rect.x:
+                self.direction = -1
+
+        for player in self.players:
+            if Entity.rect.colliderect(player.rect):
+                player.health = 0
+                time.sleep(0.25)

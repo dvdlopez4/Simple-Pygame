2a36415e4a08bd590b844659a56bce7c72aca6ae
David Lopez <dvdlopez4@gmail.com>
2023-07-14 22:39:43 -0700

    WIP

---
+++ src/Entity/Player/PlayerStates/PlayerStateManager.py
@@ -0,0 +1,39 @@
+from .StandingState import StandingState
+from .AttackState import AttackState
+from .RunningState import RunningState
+from .JumpState import JumpState
+from .DashState import DashState
+from Entity.entity import Entity
+
+
+class PlayerStateManager(object):
+    def __init__(self):
+        super(PlayerStateManager, self).__init__()
+        self.state = StandingState()
+
+    def get_state(self, state_name: str):
+        if (state_name == ""):
+            return None
+
+        if (state_name == "Standing"):
+            return StandingState()
+        if (state_name == "Attack"):
+            return AttackState()
+        if (state_name == "Dash"):
+            return DashState()
+        if (state_name == "Running"):
+            return RunningState()
+        if (state_name == "Jump"):
+            return JumpState()
+
+    def update(self, Entity: Entity):
+        if Entity.input is None:
+            return
+
+        next_state = self.state.handleInput(Entity, self)
+        if next_state is not None:
+            self.state.exit(Entity, self)
+            self.state = next_state
+            self.state.enter(Entity, self)
+
+        self.state.update(Entity)

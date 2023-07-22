ceaf06646ac91b5fdab0944c6ae242fdce0bf98c
David Lopez <dvdlopez4@gmail.com>
2023-07-22 11:58:49 -0700

    Fix dashing to the exit bug

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from World import *
-import Entity.Player.player
+from World import World
+
 
 def main():
     world = World()
     world.run()
 
     return 0
+
 
 if __name__ == '__main__':
     main()
